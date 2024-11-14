14b6ad80efd2f82b36a94f64561b06a3b3157d37
juanc <juancornejo0045@gmail.com>
2024-11-14 08:40:14 -0300

    Login finalizado

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -35,6 +35,10 @@
             panel1 = new Panel();
             pictureBox3 = new PictureBox();
             panel2 = new Panel();
+            btnIniciar = new Button();
+            label2 = new Label();
+            txtbUser = new TextBox();
+            txtbPassword = new TextBox();
             ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
             ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
             ((System.ComponentModel.ISupportInitialize)pictureBox3).BeginInit();
@@ -43,7 +47,7 @@
             // pictureBox1
             // 
             pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
-            pictureBox1.Location = new Point(144, 24);
+            pictureBox1.Location = new Point(138, 12);
             pictureBox1.Name = "pictureBox1";
             pictureBox1.Size = new Size(100, 120);
             pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
@@ -53,20 +57,20 @@
             // label1
             // 
             label1.AutoSize = true;
-            label1.Font = new Font("Bauhaus 93", 24F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.Font = new Font("Bahnschrift Condensed", 36F, FontStyle.Bold, GraphicsUnit.Point, 0);
             label1.ForeColor = SystemColors.ControlDarkDark;
-            label1.Location = new Point(50, 147);
+            label1.Location = new Point(48, 160);
             label1.Name = "label1";
-            label1.Size = new Size(286, 45);
+            label1.Size = new Size(289, 72);
             label1.TabIndex = 1;
-            label1.Text = "INICIO SESIÓN";
+            label1.Text = "INICIA SESIÓN";
             // 
             // pictureBox2
             // 
             pictureBox2.Image = (Image)resources.GetObject("pictureBox2.Image");
-            pictureBox2.Location = new Point(22, 250);
+            pictureBox2.Location = new Point(22, 270);
             pictureBox2.Name = "pictureBox2";
-            pictureBox2.Size = new Size(50, 50);
+            pictureBox2.Size = new Size(30, 30);
             pictureBox2.SizeMode = PictureBoxSizeMode.Zoom;
             pictureBox2.TabIndex = 2;
             pictureBox2.TabStop = false;
@@ -82,9 +86,9 @@
             // pictureBox3
             // 
             pictureBox3.Image = (Image)resources.GetObject("pictureBox3.Image");
-            pictureBox3.Location = new Point(22, 364);
+            pictureBox3.Location = new Point(22, 356);
             pictureBox3.Name = "pictureBox3";
-            pictureBox3.Size = new Size(50, 50);
+            pictureBox3.Size = new Size(30, 30);
             pictureBox3.SizeMode = PictureBoxSizeMode.Zoom;
             pictureBox3.TabIndex = 2;
             pictureBox3.TabStop = false;
@@ -92,24 +96,79 @@
             // panel2
             // 
             panel2.BackColor = SystemColors.ControlDarkDark;
-            panel2.Location = new Point(22, 420);
+            panel2.Location = new Point(22, 392);
             panel2.Name = "panel2";
             panel2.Size = new Size(330, 1);
             panel2.TabIndex = 3;
+            // 
+            // btnIniciar
+            // 
+            btnIniciar.BackColor = SystemColors.ControlDarkDark;
+            btnIniciar.FlatAppearance.BorderSize = 0;
+            btnIniciar.FlatStyle = FlatStyle.Flat;
+            btnIniciar.Font = new Font("Bahnschrift", 13.8F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btnIniciar.ForeColor = Color.White;
+            btnIniciar.Location = new Point(22, 449);
+            btnIniciar.Name = "btnIniciar";
+            btnIniciar.Size = new Size(330, 41);
+            btnIniciar.TabIndex = 4;
+            btnIniciar.Text = "Iniciar";
+            btnIniciar.UseVisualStyleBackColor = false;
+            btnIniciar.Click += btnIniciar_Click;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Microsoft Sans Serif", 10.2F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label2.Location = new Point(322, 12);
+            label2.Name = "label2";
+            label2.Size = new Size(48, 20);
+            label2.TabIndex = 5;
+            label2.Text = "Salir";
+            label2.Click += label2_Click;
+            // 
+            // txtbUser
+            // 
+            txtbUser.BackColor = SystemColors.ControlLight;
+            txtbUser.BorderStyle = BorderStyle.None;
+            txtbUser.Font = new Font("Microsoft Sans Serif", 10.8F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            txtbUser.ForeColor = SystemColors.ControlDarkDark;
+            txtbUser.Location = new Point(58, 276);
+            txtbUser.Multiline = true;
+            txtbUser.Name = "txtbUser";
+            txtbUser.Size = new Size(294, 24);
+            txtbUser.TabIndex = 6;
+            // 
+            // txtbPassword
+            // 
+            txtbPassword.BackColor = SystemColors.ControlLight;
+            txtbPassword.BorderStyle = BorderStyle.None;
+            txtbPassword.Font = new Font("Microsoft Sans Serif", 10.8F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            txtbPassword.ForeColor = SystemColors.ControlDarkDark;
+            txtbPassword.Location = new Point(58, 362);
+            txtbPassword.Multiline = true;
+            txtbPassword.Name = "txtbPassword";
+            txtbPassword.PasswordChar = '*';
+            txtbPassword.Size = new Size(294, 24);
+            txtbPassword.TabIndex = 7;
             // 
             // LoginForm
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = SystemColors.ControlLight;
-            ClientSize = new Size(382, 553);
+            ClientSize = new Size(382, 521);
+            Controls.Add(txtbPassword);
+            Controls.Add(txtbUser);
+            Controls.Add(label2);
+            Controls.Add(btnIniciar);
             Controls.Add(panel2);
             Controls.Add(panel1);
             Controls.Add(pictureBox3);
             Controls.Add(pictureBox2);
             Controls.Add(label1);
             Controls.Add(pictureBox1);
-            FormBorderStyle = FormBorderStyle.FixedSingle;
+            FormBorderStyle = FormBorderStyle.None;
             Icon = (Icon)resources.GetObject("$this.Icon");
             Name = "LoginForm";
             StartPosition = FormStartPosition.CenterScreen;
@@ -129,5 +188,9 @@
         private Panel panel1;
         private PictureBox pictureBox3;
         private Panel panel2;
+        private Button btnIniciar;
+        private Label label2;
+        private TextBox txtbUser;
+        private TextBox txtbPassword;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -6,5 +6,26 @@ namespace TestLogin
         {
             InitializeComponent();
         }
+        private void label2_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void btnIniciar_Click(object sender, EventArgs e)
+        {
+            if (txtbUser.Text == "test" && txtbPassword.Text == "1234")
+            {
+                new AdminForm().Show();
+                this.Hide();
+            }
+            else
+            {
+                MessageBox.Show("El usuario o contraseña es incorrecto, inténtelo nuevamente."
+                    ,"Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                txtbUser.Clear();
+                txtbPassword.Clear();
+                txtbUser.Focus();
+            }
+        }
     }
 }
